76220c087ecdde4168b82ee59edb54aa3af0b895
Collins Oduor <oduorotieno1304@gmail.com>
2021-04-05 22:44:04 +0300

    Added slope chart, day 5

---
+++ README.md
@@ -0,0 +1,4 @@
+I decided to take part in the Twitter 30DayChartChallenge.
+
+I am also learning R currently, so i hope this will accelerate my learning.
+
---
+++ slope.R
@@ -0,0 +1,26 @@
+library(ggplot2)
+
+points <- rep(c('lowest', 'highest'), 3)
+countries <- c(rep('Kenya', 2), rep('Uganda', 2), rep('Tanzania',2), rep('Rwanda', 2), rep('Burundi', 2), rep('Ethiopia', 2), 
+               rep('South Sudan', 2), rep('Eritrea', 2), rep('Somalia', 2))
+values <- c(0, 5197, 621, 5110, 0, 5895, 950, 4507, 772, 2684, -125, 4533, 381, 3187, -75, 3018, 0, 2460 )
+
+terrain <- data.frame(points, countries, values)
+
+terrain
+
+p <- function(){
+  ggplot(data = terrain, aes(x = points, y = values, group = countries)) +
+    geom_line(aes(color = countries), size = 2) +
+    geom_point(aes(color = countries), size = 4) +
+    labs(
+      title = "East Africa Highest and Lowest points",
+      subtitle = "(All values in meters)"
+    ) + 
+    xlab("Highest and lowest points") +
+    ylab("Height")
+  
+  ggsave(filename = "./eaTerrain.png")
+}
+
+p()
